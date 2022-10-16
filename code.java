8ca432ae7f2e141b001ab92c4f729441c4f5f930
Sushantfox <115439873+Sushantfox@users.noreply.github.com>
2022-10-16 11:21:41 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CodesCracker
+{
+   public static void main(String[] args)
+   {
+      Scanner s = new Scanner(System.in);
+      
+      System.out.print("Enter the Value of p: ");
+      float p = s.nextFloat();
+      System.out.print("Enter the Value of r: ");
+      float r = s.nextFloat();
+      System.out.print("Enter the Value of t: ");
+      float t = s.nextFloat();
+      System.out.print("Enter the Value of n: ");
+      float n = s.nextFloat();
+    
+      
+      System.out.println("\nAmount = " +amount);
+      System.out.println("Compound Interest = " +ci);
+   }
+}
